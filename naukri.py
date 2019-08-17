6247f427e843e0d946b49b1b7dd1a055a4fe9766
ch <chaithanya319@outlook.com>
2019-08-17 17:17:30 +0530

    3rd change

--- naukri.py
+++ naukri.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-user = "chaithanyapl57@gmail.com"
-pwd = "Iloveu5@amma"
+user = "l57@gmail.com"
+pwd = "909"
 driver = webdriver.Chrome(executable_path=r"D:\\New folder\\chromedriver.exe")
 driver.maximize_window()
 driver.get("https://www.naukri.com/")
@@ -17,7 +17,7 @@ elem = driver.find_element_by_id("pLogin")
 elem.send_keys(pwd)
 elem = driver.find_element_by_xpath("//*[@value='Login']")
 elem.click()
-print("success")
+
 '''elem = driver.find_element_by_id("usernameField")
 elem.send_keys(user)
 elem = driver.find_element_by_id("passwordField")
@@ -29,4 +29,3 @@ elem.click()
 filepath = "C://Users//ganesh//Documents//chaithu2019_17-Mar-19_21_08_16.doc"
 driver.find_element_by_xpath("//*[@class='uploadContainer']/input").send_keys(filepath)
 driver.find_element_by_xpath('//html/body/button').click()
-print("success")
